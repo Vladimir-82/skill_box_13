83ee0d6286b19ae424205ac66a04a52176473104
vladimir <grun_gespenst@tut.by>
2021-03-17 22:30:51 +0300

    telebot_continius

--- probe.py
+++ probe.py
@@ -6,7 +6,12 @@ bot = telebot.TeleBot(_token)
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться,{message.from_user.first_name}')
-    
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     # if message.text.lower() == 'привет':
@@ -19,7 +24,7 @@ def get_text_messages(message):
     responce = requests.get('http://ru.wikipedia.org')
 
     if res in message.text.lower():
-        bot.send_message(message.from_user.id, responce)
+        bot.send_message(message.from_user.id, responce.text)
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, тебя, дружище!')
 bot.polling(none_stop = True)
