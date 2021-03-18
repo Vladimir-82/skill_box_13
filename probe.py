e2493d1019ca4c5489b127470b0d9c8898ba9d16
Vladimir-82 <61869738+Vladimir-82@users.noreply.github.com>
2021-03-18 16:44:51 +0300

    telebot_progress

--- probe.py
+++ probe.py
@@ -7,9 +7,9 @@ bot = telebot.TeleBot(_token)
 def send_welcome(message):
     bot.reply_to(message, f'Я бот. Приятно познакомиться,{message.from_user.first_name}')
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
 
 
 @bot.message_handler(content_types=['text'])
@@ -20,11 +20,19 @@ def get_text_messages(message):
     #     bot.send_message(message.from_user.id, 'Жыве вечна!')
     # elif 'лукашенко' in message.text.lower().split():
     #     bot.send_message(message.from_user.id, 'хай здохне')
-    res = 'добро'
-    responce = requests.get('http://ru.wikipedia.org')
+    inq = message.text
+    responce = requests.get('http://ru.wikipedia.org/wiki' + '/' + str(inq))
 
-    if res in message.text.lower():
-        bot.send_message(message.from_user.id, responce.text)
+    if inq in responce.text:
+        bot.send_message(message.from_user.id, responce.text[responce.text.index('<title>') + len('<title>'):responce.text.index('</title>')])
+
+        var = responce.text[responce.text.index('<p>') + len('<p>'):responce.text.index('</p>')]
+        var_exit = ''
+        for elem in var:
+            if elem == '<':
+                var_exit += var[var.index('<'):]
+
+        bot.send_message(message.from_user.id,)
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, тебя, дружище!')
 bot.polling(none_stop = True)
